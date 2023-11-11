7f0b66181c52438f0b5fa9ee5d5d9603cd7de87a
Dev-Vasudevan <119096133+Dev-Vasudevan@users.noreply.github.com>
2023-11-11 12:05:45 +0530

    Add files via upload

--- IDA_proj/main.py
+++ IDA_proj/main.py
@@ -4,39 +4,48 @@ from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error
 import matplotlib.pyplot as plt
 
-df = pd.read_excel('IDA STOCK 1.xlsx')
-x = df[['Open', 'High', 'Low', 'Volume']]
-y = df['Close']
 
-# Split the data into training and testing sets
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.25, shuffle=False)
+def Cleaning (file_path):
+    df=pd.read_excel(file_path)
+    x = df[['Open', 'High', 'Low', 'Volume']]
+    y = df['Close']
+    return (df,x,y)
+
+df,x,y= Reader('IDA STOCK 1.xlsx')
+
+# df = pd.read_excel('IDA STOCK 1.xlsx')
+
+
 
 model = LinearRegression()
-model.fit(x_train, y_train)
+model.fit(x, y)
 
-print(model.score(x_test, y_test) * 100)
+def Tester(file_path):
+    df,x,y=Reader(file_path)
+    # Make predictions on the testing set
+    y_pred = model.predict(x)
+    print(model.score(x, y) * 100)
 
-# Make predictions on the testing set
-y_pred = model.predict(x_test)
+    # Evaluate the model performance
+    mse = mean_squared_error(y, y_pred)
+    print('Mean squared error:', mse)
 
-# Evaluate the model performance
-mse = mean_squared_error(y_test, y_pred)
-print('Mean squared error:', mse)
+    # Convert 'Date' to datetime
+    dates = pd.to_datetime(df['Date'])
+    # dates_train, dates_test = train_test_split(dates, test_size=0.25, shuffle=False)
+    ax = plt.axes()
+    ax.set_facecolor("black")
 
-# Convert 'Date' to datetime
-dates = pd.to_datetime(df['Date'])
-dates_train, dates_test = train_test_split(dates, test_size=0.25, shuffle=False)
-
-# Plotting the results
-plt.plot(dates_test, y_test, label='Actual Close Price')
-plt.plot(dates_test, y_pred, color="pink", label='Predicted Close Price')
-plt.xlabel('Date')
-plt.ylabel('Close Price')
-plt.title('Actual vs Predicted Close Price Over Time')
-plt.legend()
-plt.gcf().autofmt_xdate()
-plt.show()
+    # Plotting the results
+    plt.plot(dates, y,color="cyan" ,label='Actual Close Price')
+    plt.plot(dates, y_pred, color="#FC0FC0", label='Predicted Close Price')
+    plt.xlabel('Date')
+    plt.ylabel('Close Price')
+    plt.title('Actual vs Predicted Close Price Over Time')
+    plt.legend()
+    plt.gcf().autofmt_xdate()
+    plt.show()
 
 
-# plt.scatter(dates_test,y_test,marker='*')
-
+Tester("stock2.xlsx")
+Tester("stock3.xlsx")
