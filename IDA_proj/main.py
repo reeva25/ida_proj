50797572f712079ef04d79100daadfcb3d6e44fc
Dev-Vasudevan <119096133+Dev-Vasudevan@users.noreply.github.com>
2023-11-15 19:19:54 +0530

    Add files via upload

--- IDA_proj/main.py
+++ IDA_proj/main.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error
@@ -7,28 +8,42 @@ import matplotlib.pyplot as plt
 
 def Cleaning (file_path):
     df=pd.read_excel(file_path)
+    mean_volume = df['Volume'].mean()
+    print(f"Mean volume is {mean_volume}")
+
+    q1 = df['Volume'].quantile(0.25)
+    q3 = df['Volume'].quantile(0.75)
+    iqr = q3 - q1
+    upper_bound = q3 + 1.5 * iqr
+    lower_bound = q1 - 1.5 * iqr
+    # # df=df.drop((df['Volume']>upper_bound)|(df['Volume']<lower_bound))
+    df = df[(df['Volume'] < upper_bound) & (df['Volume'] > lower_bound)]
+    df=df[(np.abs(df['Volume']) < (3 * df['Volume'].std()))]
+    df = df.dropna()
+    df=df.drop_duplicates()
     x = df[['Open', 'High', 'Low', 'Volume']]
     y = df['Close']
     return (df,x,y)
 
-df,x,y= Reader('IDA STOCK 1.xlsx')
+# df,x,y= Cleaning('IDA STOCK 1.xlsx')
 
 # df = pd.read_excel('IDA STOCK 1.xlsx')
+def Trainer(file_path):
+    model = LinearRegression()
+    df, x, y = Cleaning(file_path)
+    model.fit(x, y)
+    return model
 
+def Tester(file_path , model):
+    df,x,y=Cleaning(file_path)
 
-
-model = LinearRegression()
-model.fit(x, y)
-
-def Tester(file_path):
-    df,x,y=Reader(file_path)
     # Make predictions on the testing set
     y_pred = model.predict(x)
-    print(model.score(x, y) * 100)
+    print(f"Model Score is {model.score(x, y) * 100}")
 
     # Evaluate the model performance
     mse = mean_squared_error(y, y_pred)
-    print('Mean squared error:', mse)
+    print(f'Mean squared error:{mse} \n')
 
     # Convert 'Date' to datetime
     dates = pd.to_datetime(df['Date'])
@@ -38,7 +53,7 @@ def Tester(file_path):
 
     # Plotting the results
     plt.plot(dates, y,color="cyan" ,label='Actual Close Price')
-    plt.plot(dates, y_pred, color="#FC0FC0", label='Predicted Close Price')
+    plt.plot(dates, y_pred, color="yellow", label='Predicted Close Price')
     plt.xlabel('Date')
     plt.ylabel('Close Price')
     plt.title('Actual vs Predicted Close Price Over Time')
@@ -47,5 +62,7 @@ def Tester(file_path):
     plt.show()
 
 
-Tester("stock2.xlsx")
-Tester("stock3.xlsx")
+model=Trainer("IDA STOCK 1.xlsx")
+Tester("IDA STOCK 1.xlsx",model)
+Tester("stock2.xlsx",model)
+Tester("stock3.xlsx",model)
